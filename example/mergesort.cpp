039f9fe4394a0b9770085081344b96bf4eb1e194
chara001-admin <molagon94@gmail.com>
2025-03-13 20:59:45 +0800

    Complete merge sort

--- example/mergesort.cpp
+++ example/mergesort.cpp
@@ -3,22 +3,19 @@ using namespace std;
 
 vector<int> Merge_Sort(vector<int> origin, int length){
     //split
-    if(length <= 1){
+    if(length <= 1){ //分割到最小單位
         return origin;
     }
-    vector<int> left;
-    vector<int> right;
-    int mid = length / 2;
-    left = origin.assign(origin.begin(), mid - 1);
-    right = origin.assign(mid, origin.end());
+    int mid = length / 2; //取中間位置
+    vector<int> left(origin.begin(), origin.begin() + mid);
+    vector<int> right(origin.begin() + mid, origin.end());
     left = Merge_Sort(left, left.size());
     right = Merge_Sort(right, right.size());
     //Merge
     vector<int> result;
     int pleft = 0;
     int pright = 0;
-    while(true){
-    	if(pleft >= left.size() && pright >= right.size()) break;
+    while(pleft < left.size() && pright < right.size()){
     	if(left[pleft] < right[pright]){
     		result.push_back(left[pleft]);
     		pleft++;
@@ -41,9 +38,9 @@ vector<int> Merge_Sort(vector<int> origin, int length){
 
 int main(){
     vector<int> v = {2, 5, 3, 1, 4};
-    Merge_Sort(v, v.size());
+    v = Merge_Sort(v, v.size());
     for(int i = 0; i < v.size(); i++){
-
+        cout << v[i] << " ";
     }
     return 0;
 }
